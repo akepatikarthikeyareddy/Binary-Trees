022790a1bc15811238fbb5533d65a0e3992ef9c5
Akepati Karthikeya Reddy <akepatikarthikeyareddy@gmail.com>
2022-12-20 22:44:09 +0530

    Update LowestCommonAncestor.java

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -36,13 +36,13 @@ class TreeNode{
 
 class Tree{
     static TreeNode lca(TreeNode root, int n1,int n2) {
-	    if(root == null || root.data == n1 || root.data == n2) return root;
-	    TreeNode left = lca(root.left, n1, n2);
-	    TreeNode right = lca(root.right, n1, n2);
-	    if(left == null) return right;
-	    else if(right == null) return left;
-	    else return root;
-	}
+	if(root == null || root.data == n1 || root.data == n2) return root;
+	TreeNode left = lca(root.left, n1, n2);
+	TreeNode right = lca(root.right, n1, n2);
+	if(left == null) return right;
+	else if(right == null) return left;
+	else return root;
+   }
 }
 
 /*
