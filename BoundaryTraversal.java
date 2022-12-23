ab3b7b00118098dc1a3603dfc7712b0873c3dcd3
Akepati Karthikeya Reddy <akepatikarthikeyareddy@gmail.com>
2022-12-23 22:18:41 +0530

    Update BoundaryTraversal.java

--- BoundaryTraversal.java
+++ BoundaryTraversal.java
@@ -6,8 +6,8 @@ Input:             1
               2         3
             /   \      /  \
           4       5  6      7
-		        /      \
-		      8          9
+		         /     \
+		       8         9
         
 Output: The Boundary Traversal Is: [1, 2, 4, 8, 9, 7, 3]
 */
